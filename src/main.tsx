81fbbb3e7782e75c56b2c792b0330ba3327ced24
Samuel Bumbar <134549221+samibumbar@users.noreply.github.com>
2024-10-04 21:27:42 +0200

    Udpated files

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./app";
 
 // Render your React component instead
 const root = createRoot(document.getElementById("app")!);
