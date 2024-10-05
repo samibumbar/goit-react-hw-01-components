11564fda3d14469fdaadc0e6dec31b131ce4d92a
Samuel Bumbar <134549221+samibumbar@users.noreply.github.com>
2024-10-05 22:01:54 +0200

    add proiect pages

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
-export * from "./button";
+export * from "./profile";
+export * from "./statistics";
+export * from "./friend-list";
+export * from "./transaction-history";
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./app";
+import "./style.css";
 
 // Render your React component instead
 const root = createRoot(document.getElementById("app")!);
